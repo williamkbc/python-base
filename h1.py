edbff3eb1a9e4eb013a17675c3b6d246badd06ab
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 <williamkbc@gmail.com>
2025-07-15 17:08:16 -0300

    H1 shebang configurado

--- h1.py
+++ h1.py
@@ -1,2 +1,8 @@
-print('will'.upper())
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# O programa deve imprimir o seguinte texto na tela:
+
+print('Hello, World!')
+print('will'.upper())       # coloca texto em maiúsculas
+print('Will'.lower())       # coloca texto em minúsculas
 print(56 + 39)
