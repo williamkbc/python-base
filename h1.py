ce7c15c98c5b59ccb88c685e7233c098eec162ac
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 <williamkbc@gmail.com>
2025-07-16 11:13:52 -0300

    Inclusao do arquivo 'Criando um programa q le variaveis'

--- h1.py
+++ h1.py
@@ -55,5 +55,57 @@ elif current_language == "hi_IN":
     msg = "नमस्ते, दुनिया!"
 elif current_language == "tr_TR":
     msg = "Merhaba, Dünya!"
+elif current_language == "pt_PT":
+    msg = "Olá, Mundo!"
+elif current_language == "nl_NL":
+    msg = "Hallo, Wereld!"
+elif current_language == "sv_SE":
+    msg = "Hej, världen!"
+elif current_language == "pl_PL":
+    msg = "Witaj, świecie!"
+elif current_language == "da_DK":
+    msg = "Hej, verden!"
+elif current_language == "fi_FI":
+    msg = "Hei, maailma!"
+elif current_language == "no_NO":
+    msg = "Hei, verden!"
+elif current_language == "cs_CZ":
+    msg = "Ahoj, světe!"
+elif current_language == "hu_HU":
+    msg = "Helló, világ!"
+elif current_language == "ro_RO":
+    msg = "Salut, lume!"
+elif current_language == "bg_BG":
+    msg = "Здравей, свят!"
+elif current_language == "el_GR":
+    msg = "Γειά σου, Κόσμε!"
+elif current_language == "th_TH":
+    msg = "สวัสดี, โลก!"
+elif current_language == "vi_VN":
+    msg = "Xin chào, Thế giới!"
+elif current_language == "id_ID":
+    msg = "Halo, Dunia!"
+elif current_language == "ms_MY":
+    msg = "Halo, Dunia!"
+elif current_language == "tl_PH":
+    msg = "Kamusta, Mundo!"
+elif current_language == "uk_UA":           
+    msg = "Привіт, Світ!"
+elif current_language == "fa_IR":
+    msg = "سلام، دنیا!"
+elif current_language == "iw_IL":
+    msg = "שלום, עולם!"
+elif current_language == "sr_RS":
+    msg = "Здраво, Свете!"
+elif current_language == "sk_SK":
+    msg = "Ahoj, svet!"
+elif current_language == "lt_LT":                                               
+    msg = "Sveikas, pasauli!"
+elif current_language == "lv_LV":
+    msg = "Sveika, pasaule!"
+elif current_language == "et_EE":
+    msg = "Tere, maailm!"
+
+# Exibe a mensagem de saudação na língua configurada
 
 print(msg)
