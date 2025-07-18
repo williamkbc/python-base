969d3e9c463e31652ff4bf1dbf8d87c8565bf92e
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 <williamkbc@gmail.com>
2025-07-18 12:30:50 -0300

    Código refatorado com dicionários

--- h1.py
+++ h1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """H1 Multi Language Hello World.
 Este programa imprime uma mensagem de saudação em várias línguas.
 Dependendo da lingua configurada no sistema, 
@@ -19,7 +19,7 @@ Execução:
 Requisitos:
     - Python 3.x instalado
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "William L Santos"
 __license__ = "Unlicense"
 # Dunder signifies "double underscore" e é usado para denotar métodos especiais em Python.
@@ -29,83 +29,55 @@ import os
 
 current_language = os.getenv("LANG")[:5] # Obtém a variável de ambiente LANG do sistema
 
-msg = "Hello, World!"
+# sets (Hash Table) - O(1) - Constante
+# dicts (Hash Table) - O(1) - Constante
+# Ordem de complexidade O(n) - Verifica a variável de ambiente LANG
+# e define a mensagem de saudação de acordo com o idioma configurado.
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_ES":
-    msg = "¡Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour, le monde!"
-elif current_language == "de_DE":
-    msg = "Hallo, Welt!"
-elif current_language == "ja_JP":
-    msg = "こんにちは、世界！"
-elif current_language == "zh_CN":
-    msg = "你好，世界！"
-elif current_language == "ru_RU":
-    msg = "Привет, мир!"
-elif current_language == "ar_SA":
-    msg = "مرحبا بالعالم!"
-elif current_language == "ko_KR":
-    msg = "안녕하세요, 세계!"
-elif current_language == "hi_IN":
-    msg = "नमस्ते, दुनिया!"
-elif current_language == "tr_TR":
-    msg = "Merhaba, Dünya!"
-elif current_language == "pt_PT":
-    msg = "Olá, Mundo!"
-elif current_language == "nl_NL":
-    msg = "Hallo, Wereld!"
-elif current_language == "sv_SE":
-    msg = "Hej, världen!"
-elif current_language == "pl_PL":
-    msg = "Witaj, świecie!"
-elif current_language == "da_DK":
-    msg = "Hej, verden!"
-elif current_language == "fi_FI":
-    msg = "Hei, maailma!"
-elif current_language == "no_NO":
-    msg = "Hei, verden!"
-elif current_language == "cs_CZ":
-    msg = "Ahoj, světe!"
-elif current_language == "hu_HU":
-    msg = "Helló, világ!"
-elif current_language == "ro_RO":
-    msg = "Salut, lume!"
-elif current_language == "bg_BG":
-    msg = "Здравей, свят!"
-elif current_language == "el_GR":
-    msg = "Γειά σου, Κόσμε!"
-elif current_language == "th_TH":
-    msg = "สวัสดี, โลก!"
-elif current_language == "vi_VN":
-    msg = "Xin chào, Thế giới!"
-elif current_language == "id_ID":
-    msg = "Halo, Dunia!"
-elif current_language == "ms_MY":
-    msg = "Halo, Dunia!"
-elif current_language == "tl_PH":
-    msg = "Kamusta, Mundo!"
-elif current_language == "uk_UA":           
-    msg = "Привіт, Світ!"
-elif current_language == "fa_IR":
-    msg = "سلام، دنیا!"
-elif current_language == "iw_IL":
-    msg = "שלום, עולם!"
-elif current_language == "sr_RS":
-    msg = "Здраво, Свете!"
-elif current_language == "sk_SK":
-    msg = "Ahoj, svet!"
-elif current_language == "lt_LT":                                               
-    msg = "Sveikas, pasauli!"
-elif current_language == "lv_LV":
-    msg = "Sveika, pasaule!"
-elif current_language == "et_EE":
-    msg = "Tere, maailm!"
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_ES": "¡Hola, Mundo!",
+    "fr_FR": "Bonjour, le monde!",  
+    "de_DE": "Hallo, Welt!",
+    "ja_JP": "こんにちは、世界！",        
+    "zh_CN": "你好，世界！",
+    "ru_RU": "Привет, мир!",    
+    "ar_SA": "مرحبا بالعالم!",
+    "ko_KR": "안녕하세요, 세계!",   
+    "hi_IN": "नमस्ते, दुनिया!",
+    "tr_TR": "Merhaba, Dünya!",
+    "pt_PT": "Olá, Mundo!",
+    "nl_NL": "Hallo, Wereld!",
+    "sv_SE": "Hej, världen!",
+    "pl_PL": "Witaj, świecie!",
+    "da_DK": "Hej, verden!",
+    "fi_FI": "Hei, maailma!",
+    "no_NO": "Hei, verden!",    
+    "cs_CZ": "Ahoj, světe!",
+    "hu_HU": "Helló, világ!",
+    "ro_RO": "Salut, lume!",
+    'bg_BG': "Здравей, свят!",  
+    "el_GR": "Γειά σου, Κόσμε!",
+    "th_TH": "สวัสดี, โลก!",
+    "vi_VN": "Xin chào, Thế giới!",
+    "id_ID": "Halo, Dunia!",
+    "ms_MY": "Halo, Dunia!",
+    "tl_PH": "Kamusta, Mundo!",
+    "uk_UA": "Привіт, Світ!",
+    "fa_IR": "سلام، دنیا!",
+    "iw_IL": "שלום, עולם!",
+    "sr_RS": "Здраво, Свете!",
+    "sk_SK": "Ahoj, svet!",
+    "lt_LT": "Sveikas, pasauli!", 
+    "lv_LV": "Sveika, pasaule!", 
+    "et_EE": "Tere, maailm!"
+
+
+}
+
 
 # Exibe a mensagem de saudação na língua configurada
-
-print(msg)
+# O(1) - Constante
+print(msg[current_language])
